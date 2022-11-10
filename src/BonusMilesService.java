32d2b4dc86bc6a1b2bd4278cce9727372190e0a1
Sergey Petrenko <Stevo98@mail.ru>
2022-11-10 10:54:21 +1000

    second commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class BonusMilesService {
+public class BonusMilesService {
+
+    public int calculate (int cost) {
+        int result = cost/20;
+        return result;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println("Ваши бонусные мили - " + miles);
     }
 }
